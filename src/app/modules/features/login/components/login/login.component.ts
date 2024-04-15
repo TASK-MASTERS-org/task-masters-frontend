131acfa20da402ccf68ca94a138c886bef97f368
IT20653118 <it20653118@my.sliit.lk>
2024-04-15 21:47:26 +0530

    reg integration

--- src/app/modules/features/login/components/login/login.component.ts
+++ src/app/modules/features/login/components/login/login.component.ts
@@ -1,27 +1,62 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedDataService } from '../../../../core/services/shared-data.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ValidationService } from '../../../../core/services/validation.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-
-  isForgotPassEvent: boolean  = false;
+  isForgotPassEvent: boolean = false;
   isSendForgotPassDetails: boolean = false;
   isLoggedIn: boolean = false;
-  constructor(private router: Router, private sharedService: SharedDataService) {}
-  
+  constructor(
+    private router: Router,
+    private sharedService: SharedDataService,
+    private validations: ValidationService
+  ) {}
+
+  loginForm = new FormGroup({
+    email: new FormControl(
+      null,
+      Validators.compose([Validators.required, this.validations.validEmail()])
+    ),
+    password: new FormControl(null, Validators.required),
+  });
+
+  forgotPassDetailsForm = new FormGroup({
+    email: new FormControl(
+      null,
+      Validators.compose([Validators.required, this.validations.validEmail()])
+    ),
+  });
+
+  resetPasswordForm = new FormGroup({
+    token: new FormControl(null, Validators.required),
+    newPassword: new FormControl(null, Validators.required),
+  });
+
+  get lForm() {
+    return this.loginForm.controls;
+  }
+  get fForm() {
+    return this.forgotPassDetailsForm.controls;
+  }
+  get rForm() {
+    return this.resetPasswordForm.controls;
+  }
+
   navigateToRegister(event: Event): void {
-    if(event) {
+    if (event) {
       this.router.navigate(['/register']);
     }
   }
 
   navigateToHome(event: Event): void {
-    if(event) {
+    if (event) {
       this.isLoggedIn = true;
       this.sharedService.setData(this.isLoggedIn);
       this.router.navigate(['/homepage']);
@@ -29,22 +64,47 @@ export class LoginComponent {
   }
 
   navigateForgotPassword(event: Event): void {
-    if(event) {
+    if (event) {
       this.isForgotPassEvent = true;
     }
   }
 
-  sendForgotPassDetails(event: Event) :void {
-    if(event) {
+  sendForgotPassDetails(event: Event): void {
+    if (event) {
       this.isSendForgotPassDetails = true;
+      //call to the forgot pass API
     }
   }
 
-  forgotPassword(event:Event): void {
-    if(event) {
+  navigateToLogin(event: Event): void {
+    if (event) {
       this.isForgotPassEvent = false;
       this.isSendForgotPassDetails = false;
-      this.router.navigate(['/login'])
+      this.router.navigate(['/login']);
+    }
+  }
+
+  onSubmitLoginForm(loginFormDetails: any) {
+    if (this.loginForm.valid) {
+
+    } else {
+
+    }
+  }
+
+  onSubmitForgotPassForm(forgotPassFormDetails: any) {
+    if (this.forgotPassDetailsForm.valid) {
+
+    } else {
+      
+    }
+  }
+
+  onSubmitResetPassForm(resetPasswordFormDetails: any) {
+    if (this.resetPasswordForm.valid) {
+
+    } else {
+      
     }
   }
 }
--- src/app/modules/features/register/components/register/register.component.ts
+++ src/app/modules/features/register/components/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { RegisterService } from '../../services/register.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ValidationService } from '../../../../core/services/validation.service';
 
 @Component({
   selector: 'app-register',
@@ -7,14 +11,75 @@ import { Router } from '@angular/router';
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  constructor(
+    private router: Router,
+    private registerService: RegisterService,
+    private toastr: ToastrService,
+    private validations: ValidationService
+  ) {}
 
-  constructor(private router: Router) {}
-  
   regVictor: string = 'assets/images/register.png';
 
-  navigateToHome(event: Event): void {
-    if(event) {
-      this.router.navigate(['/homepage']);
+  registerForm = new FormGroup({
+    fname: new FormControl(null, Validators.required),
+    lname: new FormControl(null, Validators.required),
+    address: new FormControl(null, Validators.required),
+    phoneNumber: new FormControl(
+      null,
+      Validators.compose([Validators.required, this.validations.validPhone()])
+    ),
+    email: new FormControl(
+      null,
+      Validators.compose([Validators.required, this.validations.validEmail()])
+    ),
+    password: new FormControl(null, Validators.required),
+  });
+
+  get rForm() {
+    return this.registerForm.controls;
+  }
+
+  navigateToLogin(): void {
+    if (event) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  onSubmit(registerFormDetails: any) {
+    // Handle form submission
+    if (this.registerForm.valid) {
+      const registerInfo = {
+        fname: registerFormDetails?.value?.fname,
+        lname: registerFormDetails?.value?.lname,
+        address: registerFormDetails?.value?.address,
+        phoneNumber: registerFormDetails?.value?.phoneNumber,
+        email: registerFormDetails?.value?.email,
+        password: registerFormDetails?.value?.password,
+      };
+      // console.log(registerInfo)
+      this.registerService.register(registerInfo).subscribe({
+        next: (response: any) => {
+          if (response.status === 200) {
+            this.toastr.success(
+              'Registered successfully, Thank you!',
+              'Success'
+            );
+            this.navigateToLogin();
+          } else {
+            this.toastr.error(
+              'Error in registration',
+              'Error' // other error messages want to consider
+            );
+            
+          }
+        },
+        error: (error) => {
+          this.toastr.error('Error in registration', 'Error');
+          
+        },
+        
+      });
+    } else {
     }
   }
 }
